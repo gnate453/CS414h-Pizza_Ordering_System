33012c283d43c8c535100fc96e95f217c36f167e
skyegelhoff <skyegelhoff@gmail.com>
2014-10-26 17:34:03 -0600

    some test stubs and rearranging of classes

--- a4/src/cs414/groupH/a4/customer/Customer.java
+++ a4/src/cs414/groupH/a4/customer/Customer.java
@@ -1,12 +1,13 @@
 package cs414.groupH.a4.customer;
-import cs414.groupH.a4.address.Address;
+import java.util.ArrayList;
 
+import cs414.groupH.a4.address.Address;
 
 public class Customer {
 
 	private String customerId;
 	private String name;
-	private Address address;
+	private ArrayList<Address> address;
 	
 	public String getId(){
 		return customerId;
@@ -16,11 +17,11 @@ public class Customer {
 		return name;
 	}
 	
-	public Address getAddress(){
+	public ArrayList<Address> getAddress(){
 		return address;
 	}
 	
-	public Customer(String id, String iname, Address ad){
+	public Customer(String id, String iname, ArrayList<Address> ad){
 		customerId = id;
 		name = iname;
 		address = ad;
---
+++ a4/src/cs414/groupH/a4/tests/CustomerTest.java
@@ -0,0 +1,14 @@
+package cs414.groupH.a4.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CustomerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
