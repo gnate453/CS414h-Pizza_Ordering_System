77a29c3d704076f5d0ba2bd91e1f20e89dc80682
Troy <Troy@Troy-PC>
2014-10-28 01:06:08 -0600

    Added Customer Dialog. Having troubles with adding addresss though

--- a4/src/cs414/groupH/a4/customer/Customer.java
+++ a4/src/cs414/groupH/a4/customer/Customer.java
@@ -2,6 +2,7 @@ package cs414.groupH.a4.customer;
 import java.util.ArrayList;
 
 import cs414.groupH.a4.address.Address;
+import cs414.groupH.a4.payment.DebitPayment;
 
 public class Customer {
 
@@ -20,13 +21,22 @@ public class Customer {
 	public ArrayList<Address> getAddress(){
 		return address;
 	}
-	
+	public static void main(String args[]){
+		Customer c = new Customer();	
+	}
+	public Customer(){
+		new CustomerDialog(this);
+		address.add(new Address());
+	}
 	public Customer(String id, String iname, ArrayList<Address> ad){
 		customerId = id;
 		name = iname;
 		address = ad;
 	}
-	
+	public void setFields(String n) {
+		name = n;
+		System.out.println(this);
+	}
 	public String toString(){
 		return "Name: " + name +
 				"\nAddress: " + address;
