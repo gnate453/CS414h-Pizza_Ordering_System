4dd840bfa0cc00be94202f45c331b67355b4902e
Nate <gnate@rams.colostate.edu>
2014-11-20 19:08:55 -0700

    employee handler updated.

--- a5/src/cs414/groupH/a5/http/EmployeeRequestHandler.java
+++ a5/src/cs414/groupH/a5/http/EmployeeRequestHandler.java
@@ -28,7 +28,16 @@ import cs414.groupH.a5.order.Order;
 import cs414.groupH.a5.payment.Payment;
 
 public class EmployeeRequestHandler implements HttpHandler {
-		
+	
+	private static final int QUERY_TYPE = 0;
+	private static final int QUERY_ID = 1;
+	private static final int QUERY_PW = 2;
+	private static final int QUERY_KEY = 0;
+	private static final int QUERY_VAL = 1;
+	private static final int SC_OK = 200;
+	private static final int SC_NOTFOUND = 404;
+	private static final int SC_ERROR = 500;
+	
 	//this function is called when an HTTP request is made
 	@Override
 	public void handle(HttpExchange exchange) throws IOException {
@@ -37,19 +46,22 @@ public class EmployeeRequestHandler implements HttpHandler {
 		
 		String query = uri.getQuery();
 		String response = "";
+        //if there was a query it was an order request.
 		if (query != null) {
-			//if the url has parameters, we need to parse them 
-			response = parseQuery(query);
 			
-			//send the response
-			//200 means the request was successful 
-			exchange.sendResponseHeaders(200, response.length());
+			response = parseEmployeeQuery(query);
+			
+			if (response.equalsIgnoreCase("error")) {
+				exchange.sendResponseHeaders(SC_NOTFOUND, response.getBytes().length);
+			}
+			else {
+				exchange.sendResponseHeaders(SC_OK, response.getBytes().length);
+			}
 		}
 		else {
-			response = getMenuXml();
-			System.out.println(response);
-			//response = "An error occurred! Please try your request again. Sorry for the inconvenience.";
-			exchange.sendResponseHeaders(200, response.length());
+			response = "error";
+			System.out.println("Employee Handler did not find a query string in URL.");
+			exchange.sendResponseHeaders(SC_NOTFOUND, response.getBytes().length);
 		}
 
 		//output the information
@@ -59,206 +71,44 @@ public class EmployeeRequestHandler implements HttpHandler {
 	}
 	
 	//parses the parameters from the URL
-	//these are in the form {base_url}?customer=name&size=pizza_size&toppings=t1-t2-..-tn
-	private String parseQuery(String query) {
+    //Option 1: /employee?type=Login&id=empID&pw=empPW
+	//Option 2: /employee?type=Logout&id=empID
+	//Option 3: /employee?type=TypeCheck&id=empID
+	private String parseEmployeeQuery(String query) {
 		//split the query based on parameters
-		String[] subs = query.split("&");
-		String ret = "";
+		String[] subs = query.split("&");	
 		
-		for (int i=0; i<subs.length; i++) {	
-			//key is on the left and value is on the right, so we split this
-			String[] values = subs[i].split("=");
-			if (values[0].equalsIgnoreCase("type")) {
-				if (values[1].equalsIgnoreCase("typecheck")) {
-					values = subs[i+1].split("=");
-					Employee emp = EmployeeManager.findEmployee(values[1]);
-					if (emp != null) {
-						System.out.println(emp.getEmpType());
-						return emp.getEmpType().toString();
-					}
-					else {
-						System.out.println("ERROR: Employee with id '"+values[1]+"' not found.");
-					}
-				}
+		String[] type = subs[QUERY_TYPE].split("=");
+		if (type[QUERY_KEY].equalsIgnoreCase("type") && type[QUERY_VAL].equalsIgnoreCase("login")) {
+			String[] id = subs[QUERY_ID].split("=");
+			String[] pw = subs[QUERY_PW].split("=");
+			if (EmployeeManager.doesEmpExist(EmployeeManager.findEmployee(id[QUERY_VAL]))) {
+				if (EmployeeManager.verifyCreds(id[QUERY_VAL], pw[QUERY_VAL]))
+					return "VALID";
+				else
+					return "INVALID";
 			}
-			else if (values[0].equals("size")) {
-				
-			}
-			else if (values[0].equals("toppings")) {
-				
-			}
+			else
+				return "error";
 		}
-		
-		return ret;
-	}
-	
-	private Order xmlParser(String xmlInput) {
-		Customer cust = new Customer();
-		List<String> itemNames = new ArrayList<String>();
-		List<Payment> payments = new ArrayList<Payment>();
-		
-		Document dom;
-		//get the factory
-		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-		
-		try {
-			//Using factory get an instance of document builder
-			DocumentBuilder db = dbf.newDocumentBuilder();
-			
-			//parse using builder to get DOM representation of the XML file
-			dom = db.parse(xmlInput);
-			
-			
-			//get the root elememt
-			Element docEle = dom.getDocumentElement();
-			
-			//get a nodelist of <Customer> elements
-			NodeList nl = docEle.getElementsByTagName("Customer");
-			if(nl != null && nl.getLength() > 0) {
-				for(int i = 0 ; i < nl.getLength();i++) {
-					//get the customer element
-					Element el = (Element)nl.item(i);
-					
-					//get the Customer object
-					cust = getCustomer(el);
-				}
+		else if (type[QUERY_KEY].equalsIgnoreCase("type") && type[QUERY_VAL].equalsIgnoreCase("logout")) {
+			String[] id = subs[QUERY_ID].split("=");
+			if (EmployeeManager.doesEmpExist(EmployeeManager.findEmployee(id[QUERY_VAL]))) {
+				return "VALID";
 			}
-			
-			//get a nodelist of <Item> elements
-			nl = docEle.getElementsByTagName("Item");
-			if(nl != null && nl.getLength() > 0) {
-				for(int i = 0 ; i < nl.getLength();i++) {
-					//get the item element
-					Element el = (Element)nl.item(i);
-					
-					//get the MenuItem object and adds it to list
-					itemNames.add(getTextValue(el, "Name"));
-				}
+			else
+				return "error";
+		}
+		else if (type[QUERY_KEY].equalsIgnoreCase("type") && type[QUERY_VAL].equalsIgnoreCase("typecheck")) {
+			String[] id = subs[QUERY_ID].split("=");
+			if (EmployeeManager.doesEmpExist(EmployeeManager.findEmployee(id[QUERY_VAL]))) {
+				return EmployeeManager.findEmployee(id[QUERY_VAL]).getEmpType().toString();
 			}
-			
-			//get a nodelist of <Payment> elements
-			nl = docEle.getElementsByTagName("Payment");
-			if(nl != null && nl.getLength() > 0) {
-				for(int i = 0 ; i < nl.getLength();i++) {
-					//get the payment element
-					Element el = (Element)nl.item(i);
-					
-					//get the MenuItem object and adds it to list
-					payments.add(getPayment(el));
-				}
-			}
-			
-			return SystemManager.createOrder(cust, itemNames, payments);
-			
-		}catch(ParserConfigurationException pce) {
-			pce.printStackTrace();
-		}catch(SAXException se) {
-			se.printStackTrace();
-		}catch(IOException ioe) {
-			ioe.printStackTrace();
+			else
+				return "error";
 		}
-		
-		return null;
+		else
+			return "error";
 	}
-	
-	private Customer getCustomer(Element empEl) {
-		String name = getTextValue(empEl, "Name");
-		String street = getTextValue(empEl, "Street");
-		String city = getTextValue(empEl, "City");
-		String state = getTextValue(empEl, "State");
-		String zip = getTextValue(empEl, "Zip");
-		String phone = getTextValue(empEl, "Phone");
-		
-		//Create a new Customer with the value read from the xml nodes
-		Customer cust = new Customer(name, new Address(street, city, state, zip, phone));
-		
-		return cust;
-	}
-	
-	private Payment getPayment(Element empEl) {
-		double amount = getDoubleValue(empEl, "Amount");
-		String validated = getTextValue(empEl, "Validated");
-		
-		//Create a new Customer with the value read from the xml nodes
-		Payment payment = new Payment(amount, Boolean.parseBoolean(validated));
-		
-		return payment;
-	}
-	
-	private String getTextValue(Element ele, String tagName) {
-		String textVal = null;
-		NodeList nl = ele.getElementsByTagName(tagName);
-		if(nl != null && nl.getLength() > 0) {
-			Element el = (Element)nl.item(0);
-			textVal = el.getFirstChild().getNodeValue();
-		}
 
-		return textVal;
-	}
-	
-	private double getDoubleValue(Element ele, String tagName) {
-		//in production application you would catch the exception
-		return Double.parseDouble(getTextValue(ele,tagName));
-	}
-	
-	
-	//Turns the menu into an XML representation
-	private String getMenuXml() {
-		StringBuffer buffer = new StringBuffer();
-		buffer.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
-		buffer.append("<Menu>");
-		for (MenuItem item : SystemManager.getMenuItems()) {
-			buffer.append("<Item>");
-			
-			buffer.append("<Name>");
-			buffer.append(item.getName());
-			buffer.append("</Name>");
-			buffer.append("<Price>");
-			buffer.append(item.getPrice());
-			buffer.append("</Price>");
-			buffer.append("<Special>");
-			buffer.append(item.isDailySpecial());
-			buffer.append("</Special>");
-
-			buffer.append("</Item>");
-		}
-		buffer.append("</Menu>");
-
-		return buffer.toString();
-	}
-	
-	//Turns the order confirmation into XML
-	private String getOrderConfXml(Order o) {
-		StringBuffer buffer = new StringBuffer();
-		buffer.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
-		buffer.append("<order>");
-
-		buffer.append("<orderId>");
-		buffer.append(o.getOrderId());
-		buffer.append("</orderId>");
-		
-		buffer.append("<customer>");
-		buffer.append("<name>");
-		buffer.append(o.getCustomer().getName());
-		buffer.append("</name>");
-		buffer.append("<address>");
-		buffer.append(o.getCustomer().getAddress());
-		buffer.append("</address>");
-		buffer.append("</customer>");
-
-		buffer.append("<items>");
-		for (MenuItem item : o.getItems()) {
-			buffer.append("<name>");
-			buffer.append(item.getName());
-			buffer.append("</name>");
-			buffer.append("<price>");
-			buffer.append(item.getPrice());
-			buffer.append("</price>");
-		}
-		buffer.append("</items>");
-
-		buffer.append("</order>");
-
-		return buffer.toString();
-	}
 }
